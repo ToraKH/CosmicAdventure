aa8b6b4f0da4bf6c78d90a24562a0df7ae72bdc2
Tora <tohom2986@uit.no>
2025-02-13 13:29:20 +0100

    Initial commit

---
+++ data/config.py
@@ -0,0 +1,130 @@
+# Screen size
+SCREEN_X = 1200
+SCREEN_Y = 600
+BORDER = 50
+DELAY = 500
+
+# Sprite images [retrieved from opengameart.org]
+SHIP_PLAYER1_IMAGE = "data/images/spaceships/grønn.png"
+SHIP_PLAYER2_IMAGE = "data/images/spaceships/mørk_blå.png"
+SHIP_GRØNN= "data/images/spaceships/grønn.png" #Retrieved from  https://opengameart.org/content/pixel-spaceship
+SHIP_SVART = "data/images/spaceships/svart.png"
+SHIP_LYS_BLÅ = "data/images/spaceships/lys_blå.png"
+SHIP_MØRK_BLÅ = "data/images/spaceships/mørk_blå.png"
+SHIP_MØRK_GRØNN = "data/images/spaceships/mørk_grønn.png"
+SHIP_MULTI_CAVE = "data/images/spaceships/multi_cave.png"
+SHIP_MULTI_SPACE = "data/images/spaceships/multi_space.png"
+
+SHIP_GUL = "data/images/spaceships/gul2.png"
+SHIP_ORANSJ = "data/images/spaceships/oransj.png"
+SHIP_MULTI_LAVA = "data/images/spaceships/multi_lava.png"
+SHIP_RØD = "data/images/spaceships/rød.png"
+SHIP_ROSA = "data/images/spaceships/rosa.png"
+SHIP_LILLA = "data/images/spaceships/lilla.png"
+SHIP_MULTI_GUL = "data/images/spaceships/multi_gul.png"
+
+OBSTACLE_ARENA1 = "data/images/planet.png" 
+OBSTACLE_ARENA2 = "data/images/teofila.gif"   #https://www.deviantart.com/embea-icons/art/Ignition-icon-549441503
+OBSTACLE_ARENA3 = "data/images/bat.gif"      #https://www.deviantart.com/silvervoidart/art/Pixel-Bat-01-923272602
+
+
+BULLET_IMAGE = "data/images/bullet.png"
+FUEL_IMAGE = "data/images/fuel_can.png"
+POOF_IMAGE = "data/images/Poof.png" #Retrieved from https://opengameart.org/content/poof-effect-spritesheet 
+
+
+
+ARENA1 = "data/background/mushroom.jpg"
+ARENA2 = "data/background/night_forest.jpg"
+ARENA3 = "data/background/volcano_cave.jpg"
+
+ARENA_IMG1 = "data/arena/mushroom1.jpg"
+ARENA_IMG2 = "data/arena/night_forest1.jpg"
+ARENA_IMG3 = "data/arena/volcano_cave1.jpg"
+
+START = "data/background/starter.jpg"
+END = "data/background/ender.jpg"
+CUSTOMIZER = "data/background/custom.jpg"
+
+
+START_BUTTON = "data/buttons/space_to_start.png"
+RESTART_BUTTON = "data/buttons/space_to_restart.png"
+CUSTOMIZE_BUTTON = "data/buttons/g_to_customize.png"
+CONFIRM_BUTTON = "data/buttons/space_to_confirm.png"
+CHOOSE_SHIP1 = "data/buttons/2_for_skip1.png"  
+CHOOSE_SHIP2 = "data/buttons/3_for_skip2.png"    
+CHOOSE_ARENA = "data/buttons/1_for_arena.png"    
+RIGHT_ARROW = "data/buttons/right_arrow.png"     
+LEFT_ARROW = "data/buttons/left_arrow.png"     
+GOTO_START = "data/buttons/g_to_customize.png"
+CREDIT = "data/buttons/esc_to_quit.png"
+BUTTON_SIZE = 3
+
+
+
+
+# Motion controls
+GRAVITY = 1.4
+THRUST = 3.0
+ROTATION_SPEED = 5.0
+
+# Artillary controls
+BULLET_SPEED = 5.0          #How fast the bullets fly
+BULLET_COOL_DOWN = 200      #How quick bullets fire
+BULLET_MAX = 3              #How many bullet per bullet-set
+
+# Gas controls 
+FUELLIMIT = 3000    # Size of fuel tank
+FUELUSE = 1         # Amount of fuel used
+REFUEL_RATE = 100   # Amount of fuel the ship gets when refueling
+
+#Control number of obstacles
+OBSTACLES = 2
+
+# Game controls
+HEALTH = 500          #Health at the beginning
+HITPOINT = -10
+SCOREPOINT = 10
+OBS_HEALTH = 500
+
+# Music controls
+MUSIC = "data/sound/space1.mp3" # Music by Dmitrii Kolesnikov from Pixabay: https://pixabay.com/music/ambient-space-158081/ 
+VOLUME = 0.25
+
+CRASH_SOUND = "data/sound/epic_swoosh.mp3" #Sound Effect by floraphonic from Pixabay: https://pixabay.com/sound-effects/epic-swoosh-boom-1-183996/
+SHOOT_SOUND = "data/sound/shot.mp3" #Sound Effect from <a href from Pixabay: "https://pixabay.com/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=14562">Pixabay</a>
+SHOOT_VOLUME = 0.04
+# Poof images from https://opengameart.org/content/poof-effect-spritesheet
+I0 = "data/poof_images/tile000.png"
+I1 = "data/poof_images/tile001.png"
+I2 = "data/poof_images/tile002.png"
+I3 = "data/poof_images/tile003.png"
+I4 = "data/poof_images/tile004.png"
+I5 = "data/poof_images/tile005.png"
+I6 = "data/poof_images/tile006.png"
+I7 = "data/poof_images/tile007.png"
+I8 = "data/poof_images/tile008.png"
+I9 = "data/poof_images/tile009.png"
+I10 = "data/poof_images/tile010.png"
+I11 = "data/poof_images/tile011.png"
+I12 = "data/poof_images/tile012.png"
+I13 = "data/poof_images/tile013.png"
+I14 = "data/poof_images/tile014.png"
+I15 = "data/poof_images/tile015.png"
+I16 = "data/poof_images/tile016.png"
+I17 = "data/poof_images/tile017.png"
+I18 = "data/poof_images/tile018.png"
+I19 = "data/poof_images/tile019.png"
+I20 = "data/poof_images/tile020.png"
+I21 = "data/poof_images/tile021.png"
+I22 = "data/poof_images/tile022.png"
+I23 = "data/poof_images/tile023.png"
+I24 = "data/poof_images/tile024.png"
+I25 = "data/poof_images/tile025.png"
+I26 = "data/poof_images/tile026.png"
+I27 = "data/poof_images/tile027.png"
+I28 = "data/poof_images/tile028.png"
+I29 = "data/poof_images/tile029.png"
+
+#0.07 seconds between shifting poof images
+INTERVAL = 70
